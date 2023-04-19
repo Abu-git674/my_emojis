26beb7c3e841d80bbc3bf8bd47c8a01791923407
Abu <79767304+Abu-git674@users.noreply.github.com>
2023-04-19 16:42:11 -0400

    feat:add emojis

---
+++ index.js
@@ -0,0 +1,53 @@
+const myEmojis = ["👨‍💻", "⛷", "🍲"];
+const emojiContainer = document.getElementById("emoji-container");
+const emojiInput = document.getElementById("emoji-input");
+const pushBtn = document.getElementById("push-btn");
+const unShiftBtn = document.getElementById("unshift-btn");
+const popBtn = document.getElementById("pop-btn");
+const shiftBtn = document.getElementById("shift-btn");
+
+function renderEmojis(containerID, myEmojisArray) {
+  const container = containerID
+  container.innerHTML = "";
+  for (let i = 0; i < myEmojisArray.length; i++) {
+    const emoji = document.createElement("span");
+    emoji.textContent = myEmojisArray[i];
+    container.append(emoji);
+  }
+}
+
+renderEmojis(emojiContainer, myEmojis);
+
+pushBtn.addEventListener("click", function () {
+  modifyEmojis("push");
+});
+
+unShiftBtn.addEventListener("click", function () {
+  modifyEmojis("unshift");
+});
+
+popBtn.addEventListener("click", function () {
+  modifyEmojis("pop");
+});
+
+shiftBtn.addEventListener("click", function () {
+  modifyEmojis("shift");
+});
+
+function modifyEmojis(action) {
+  if (emojiInput.value) {
+    if (action === "push") {
+      myEmojis.push(emojiInput.value);
+    } else if (action === "unshift") {
+      myEmojis.unshift(emojiInput.value);
+    }
+    emojiInput.value = "";
+  }
+  if (action === "pop") {
+    myEmojis.pop(emojiInput.value);
+  } else if (action === "shift") {
+    myEmojis.shift(emojiInput.value);
+  }
+
+  renderEmojis();
+}
